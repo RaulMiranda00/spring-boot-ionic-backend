77fbf1c37e5e5a80eaaf19e8aa2e13a8306f958c
Raul Salignac Miranda <raulsalignac@gmail.com>
2020-01-27 09:13:51 -0300

    Estado e Cidade

--- src/main/java/com/curso/CursomcApplication.java
+++ src/main/java/com/curso/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.domain.Categoria;
+import com.curso.domain.Cidade;
+import com.curso.domain.Estado;
 import com.curso.domain.Produto;
 import com.curso.repositories.CategoriaRepository;
+import com.curso.repositories.CidadeRepository;
+import com.curso.repositories.EstadoRepository;
 import com.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -35,6 +45,16 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p2= new Produto(null, "Impressora", 800.00);
 		Produto p3= new Produto(null, "Mouse", 80.00);
 		
+		Estado est1= new Estado(null, "Minas Gerais");
+		Estado est2= new Estado(null, "São Paulo");
+		
+		Cidade c1= new Cidade(null, "Uberlândia", est1);
+		Cidade c2= new Cidade(null, "São Paulo", est2);
+		Cidade c3= new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
@@ -45,6 +65,11 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
+		
 	}
 	
 	
