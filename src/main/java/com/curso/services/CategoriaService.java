a6645760f83b0c83b0dacb9554a54afc0a6615e0
Raul Salignac Miranda <raulsalignac@gmail.com>
2020-03-02 19:27:30 -0300

    Paginação com parâmetros na requisição

--- src/main/java/com/curso/services/CategoriaService.java
+++ src/main/java/com/curso/services/CategoriaService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.curso.domain.Categoria;
@@ -49,6 +51,12 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+		
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage);
+		return repo.findAll(pageRequest);
 		
 	}
 	
