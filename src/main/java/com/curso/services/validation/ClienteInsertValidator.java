fb5b70f323a60fb454fbe5b292844574facf88c5
Raul Salignac Miranda <raulsalignac@gmail.com>
2020-04-01 17:30:33 -0300

    Validação customizada: email já existente na inserção de clientes

--- src/main/java/com/curso/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/curso/services/validation/ClienteInsertValidator.java
@@ -6,8 +6,12 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.curso.domain.Cliente;
 import com.curso.domain.enums.TipoCliente;
 import com.curso.dto.ClienteNewDTO;
+import com.curso.repositories.ClienteRepository;
 import com.curso.resources.exceptions.FieldMessage;
 import com.curso.services.validation.utils.BR;
 
@@ -16,18 +20,25 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public void initialize(ClienteInsert ann) {
 	}
 
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
 	public boolean isValid(ClienteNewDTO objDTO, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 
-		if(objDTO.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDTO.getCpfOuCnpj())) {
+		if (objDTO.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-		
-		if(objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
+
+		if (objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
+		Cliente aux= repo.findByEmail(objDTO.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente."));
+		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
