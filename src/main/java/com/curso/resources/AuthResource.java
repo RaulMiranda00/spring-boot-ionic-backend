ac2b594ecdec0b2a6c30670e6ab11c7dc518d73c
Raul Salignac Miranda <raulsalignac@gmail.com>
2020-05-04 23:37:44 -0300

    Esqueci a senha

--- src/main/java/com/curso/resources/AuthResource.java
+++ src/main/java/com/curso/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.curso.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.dto.EmailDTO;
 import com.curso.security.JWTUtil;
 import com.curso.security.UserSS;
+import com.curso.services.AuthService;
 import com.curso.services.UserService;
 
 @RestController
@@ -18,6 +22,10 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
+	
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -27,4 +35,9 @@ public class AuthResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+		service.sendNewPassword(objDTO.getEmail());
+		return ResponseEntity.noContent().build();
+	}
 }
